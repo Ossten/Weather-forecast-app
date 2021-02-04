f80642ef9341a1bbb0dc7a42f004abbe50625311
Osten <osten@protonmail.com>
2021-02-04 19:27:14 +0100

    Added actual location and search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,146 +1,94 @@
-import logo from './img/logo.svg';
-import React from 'react';
-import './App.css';
-import { AK } from './config.js';
-import video from './img/video.webm';
-import './weatherCard.css'
+import logo from "./img/logo.svg";
+import React from "react";
+import "./App.css";
+import { AK } from "./config.js";
+import video from "./img/video.webm";
+import { WeatherCard } from "./WeatherCard/WeatherCard.jsx";
+import { Header } from "./Header/Header";
 
 class WeatherApp extends React.Component {
   constructor(props) {
     super(props);
+
+    this.positionChange = this.positionChange.bind(this);
+
     this.state = {
       weatherData: [],
-      loc: 'eba2d4e00ab8a7f',
-      lat: 59.91273,
-      lon: 10.74609,
+      loc: "eba2d4e00ab8a7f",
+      lat: 0,
+      lon: 0,
       k: AK,
-      isLoaded: false,
-    }
+      isLoadedWeather: false,
+      isLoadedLocation: false,
+    };
+  }
+  // Position comming from header
+  positionChange(position) {
+    this.setState((state) => {
+      return {
+        ...state,
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+      };
+    });
+    // console.log("position:", position);
+    // console.log("coordionates: ", this.state.lat, this.state.lon);
+    // console.log("weather data: ", this.state.weatherData);
+
+    this.forceUpdate();
+    (() => {
+      this.componentDidMount();
+    })();
   }
 
   componentDidMount() {
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${this.state.lat}&lon=${this.state.lon}&units=metric&exclude=current,minutely,hourly,alerts&appid=${this.state.loc}${this.state.k}`)
-      .then(res => res.json())
-      .then(json => {
-        this.setState({
-          isLoaded: true,
-          weatherData: json,
-        })
-      })
-  }
-
-  date(epoch) {
-    let d = new Date(parseInt(epoch) * 1000)
-    const weekday = new Array(7);
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
-    const month = new Array(12);
-    month[0] = "Jan";
-    month[1] = "Feb";
-    month[2] = "Mar";
-    month[3] = "Apr";
-    month[4] = "May";
-    month[5] = "Jun";
-    month[6] = "Jul";
-    month[7] = "Aug";
-    month[8] = "Sep";
-    month[9] = "Oct";
-    month[10] = "Nov";
-    month[11] = "Dec";
-    return (<div>
-      <p>{weekday[d.getDay()]}</p>
-      <p>{d.getDate()}. {month[d.getMonth()]}. {d.getFullYear()}</p>
-    </div>)
-  }
-
-  getCardinalDirection(angle) {
-    const directions = ['↑ N', '↗ NE', '→ E', '↘ SE', '↓ S', '↙ SW', '← W', '↖ NW'];
-    return directions[Math.round(angle / 45) % 8];
+    fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${this.state.lat}&lon=${this.state.lon}&units=metric&exclude=current,minutely,hourly,alerts&appid=${this.state.loc}${this.state.k}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState((state) => {
+          // da se pazit na ova , ne go znaev prethodniot state --- vidi dokumnetacija
+          return {
+            ...state,
+            isLoadedWeather: true,
+            weatherData: json,
+          };
+        });
+      });
   }
 
   render() {
-    var { isLoaded, weatherData } = this.state;
-    if (!isLoaded) {
+    var { isLoadedWeather, weatherData, lon, lat } = this.state;
+    if (!isLoadedWeather) {
       return <div>Loading...</div>;
-    }
-    else {
+    } else {
+      // console.log(this.state.lat, this.state.lon);
       return (
-        <div className="weatherApp" >
+        <div className="weatherApp">
           <div id="video">
             <video className="video" autoPlay loop muted>
               <source src={video} type="video/webm" />
               <source src={video} type="video/ogg" />
-            Your browser does not support the video tag.
-          </video>
+              Your browser does not support the video tag.
+            </video>
           </div>
-          <div>
-            <header className="App-header">
-              <div>
-                <img src={logo} className="App-logo" alt="logo" />
-              </div>
-              <div className="title title_front">
-                <p >8-Day Weather Forecast</p>
-                <p>{weatherData.timezone}</p>
-              </div>
-            </header>
+          <div className="logo">
+            <div>
+              <img src={logo} className="App-logo" alt="logo" />
+            </div>
           </div>
-          <div className="cardWrapper">
-            {weatherData.daily.map(daily => {
-              return (
-                <div key={daily.dt} className="weatherCard" >
-                  <div className="weatherCard weatherCard__front">
-                    <p>{this.date(daily.dt)}</p>
-                    <content>
-                      <div>{daily.weather.map(weather => {
-                        return (
-                          <div>
-                            <img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} className="weatherIcon" alt={'aaa'} />
-                            <p>{weather.description}</p>
-                          </div>)
-                      })}
-                      </div>
-                      {daily.temp.day}°C
-                    </content>
-                  </div>
-                  <div className="weatherCard weatherCard__back">
-                    <div>{daily.weather.map(weather => {
-                      return (
-                        <div>
-                          <img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} className="weatherIcon__back" alt={'aaa'} />
-                          <p>{this.date(daily.dt)} </p>
-                        </div>
-                      )
-                    })}
-                    </div>
-                    <div className="weatherCard__back-content">
-                      <content>
-                        <div>
-                          <ul><u>Temperature</u></ul>
-                          <li>Day: {daily.temp.day}°C<br></br> - feels: {daily.feels_like.day}°C</li>
-                          <li>Night: {daily.temp.night}°C <br></br> - feels: {daily.feels_like.night}°C</li>
-                          <li>Evening: {daily.temp.eve}°C <br></br> - feels: {daily.feels_like.eve}</li>
-                          <li>Morning: {daily.temp.morn}°C <br></br> - feels: {daily.feels_like.morn}</li>
-                          <li>Minimum: {daily.temp.min}°C</li>
-                          <li>Maximum: {daily.temp.max}°C</li>
-                        </div>
-                        <div>
-                          <p>Pressure: {daily.pressure} hPa</p>
-                          <p>Humidity: {daily.humidity}%</p>
-                          <p>Wind: {daily.wind_speed}ms<sup>-1</sup> &nbsp;  {this.getCardinalDirection(daily.wind_deg)} </p>
-                        </div>
-                      </content>
-                    </div>
-                  </div>
-                </div>
-              )
-            })}
-          </div>
-        </div >
+          <Header
+            weatherData={weatherData}
+            lat={lat}
+            lon={lon}
+            handlePositionChange={this.positionChange}
+          />
+          <WeatherCard
+            // onChange={this.onChange.bind(this)}
+            weatherData={weatherData}
+          />
+        </div>
       );
     }
   }
